cee758bea791d73875c0f1f67d15f6a465200d9f
Jean Pierre <stick-master666@hotmail.com>
2020-09-16 11:52:30 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Login from "./components/Login";
+import useLocalStorage from "./hooks/useLocalStorage";
+import Dashboard from "./components/Dashboard";
+import { ContactsProvider } from "./contexts/ContactsProvider";
+
+function App() {
+  const [id, setId] = useLocalStorage("id");
+
+  const dashboard = (
+    <ContactsProvider>
+      <Dashboard id={id} />
+    </ContactsProvider>
+  );
+
+  return (
+    id ? dashboard : <Login onIdSubmit={setId} />
+  );
+}
+
+export default App;
